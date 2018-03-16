ab61584326e8676e9ea4d58f786b0d4a6d51f7b7
Blaine Carter <blaine.carter@oracle.com>
2018-03-15 21:15:03 -0600

    cleaning up liquibase test changes

--- coreDatabase/dd_tests_schema/dd_grants.sql
+++
@@ -1,5 +0,0 @@
-/* Grant access to dd and dd_non_ebr objects */
-connect dd/dd
-grant execute on dd.dd_admin_pkg to dd_tests;
-grant all on dd.dd_members to dd_tests;
-grant all on dd.dd_settings to dd_tests;
